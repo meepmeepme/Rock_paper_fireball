92811c5b9efa6771a4e353b8b4f7d029a3518d3e
meepmeepme <ianktalbert@me.com>
2016-10-03 15:22:09 -1000

    added pictures for lose and win conditions

--- js/app.js
+++ js/app.js
@@ -3,9 +3,29 @@ function buttonpush1() {
 	player =  Math.floor(Math.random()*10);
 	console.log(player);
 	if (player < 5) {
-		document.getElementById("box").innerHTML = ("You win")
+		document.getElementById("box").innerHTML = ("You win<br> The AI chose <br><img src='PIC/char.png' class='pok2'>")
 	}
 	if (player > 5) {
-		document.getElementById("box").innerHTML = ("You lose")
+		document.getElementById("box").innerHTML = ("You lose <br> The AI chose <br><img src='PIC/bulb.png' class='pok2'>")
+	}
+}
+function buttonpush2() {
+	player =  Math.floor(Math.random()*10);
+	console.log(player);
+	if (player < 5) {
+		document.getElementById("box").innerHTML = ("You win<br> The AI chose <br><img src='PIC/squr.png' class='pok2'>")
+	}
+	if (player > 5) {
+		document.getElementById("box").innerHTML = ("You lose <br> The AI chose <br><img src='PIC/char.png' class='pok2'>")
+	}
+}
+function buttonpush3() {
+	player =  Math.floor(Math.random()*10);
+	console.log(player);
+	if (player < 5) {
+		document.getElementById("box").innerHTML = ("You win<br> The AI chose <br><img src='PIC/squr.png' class='pok2'>")
+	}
+	if (player > 5) {
+		document.getElementById("box").innerHTML = ("You lose <br> The AI chose <br><img src='PIC/char.png' class='pok2'>")
 	}
 }
