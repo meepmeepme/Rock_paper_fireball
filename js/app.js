cb323f282b2c2bc2420ec9aa6516acf1df379890
meepmeepme <ianktalbert@me.com>
2016-10-03 17:12:02 -1000

    background

--- js/app.js
+++ js/app.js
@@ -8,6 +8,9 @@ function buttonpush1() {
 	if (player > 5) {
 		document.getElementById("box").innerHTML = ("You lose <br> The AI chose <br><img src='PIC/bulb.png' class='pok2'>")
 	}
+	if (player == 5) {
+		document.getElementById("box").innerHTML = ("Tie! <br> The AI chose <br><img src='PIC/squr.png' class='pok2'>")
+	}
 }
 function buttonpush2() {
 	player =  Math.floor(Math.random()*10);
@@ -18,14 +21,20 @@ function buttonpush2() {
 	if (player > 5) {
 		document.getElementById("box").innerHTML = ("You lose <br> The AI chose <br><img src='PIC/char.png' class='pok2'>")
 	}
+	if (player == 5) {
+		document.getElementById("box").innerHTML = ("Tie! <br> The AI chose <br><img src='PIC/bulb.png' class='pok2'>")
+	}
 }
 function buttonpush3() {
 	player =  Math.floor(Math.random()*10);
 	console.log(player);
 	if (player < 5) {
-		document.getElementById("box").innerHTML = ("You win<br> The AI chose <br><img src='PIC/squr.png' class='pok2'>")
+		document.getElementById("box").innerHTML = ("You win<br> The AI chose <br><img src='PIC/bulb.png' class='pok2'>")
 	}
 	if (player > 5) {
-		document.getElementById("box").innerHTML = ("You lose <br> The AI chose <br><img src='PIC/char.png' class='pok2'>")
+		document.getElementById("box").innerHTML = ("You lose <br> The AI chose <br><img src='PIC/squr.png' class='pok2'>")
+	}
+	if (player == 5) {
+		document.getElementById("box").innerHTML = ("Tie! <br> The AI chose <br><img src='PIC/char.png' class='pok2'>")
 	}
 }
